91078d0c76e82d3795d8e0e1f06b5f53e5b707ee
sagiri <i@sagiri.me>
2021-03-22 22:04:35 +0800

    qwq

--- src/main/kotlin/me/sagiri/mirai/glot/PluginMain.kt
+++ src/main/kotlin/me/sagiri/mirai/glot/PluginMain.kt
@@ -6,6 +6,7 @@ import net.mamoe.mirai.utils.info
 import com.github.kevinsawicki.http.HttpRequest
 import net.mamoe.mirai.event.events.MessageEvent
 import net.mamoe.mirai.event.globalEventChannel
+import net.mamoe.mirai.message.code.MiraiCode.deserializeMiraiCode
 import net.mamoe.mirai.message.data.Message
 import net.mamoe.mirai.message.data.content
 import org.json.JSONArray
@@ -32,6 +33,46 @@ object PluginMain : KotlinPlugin(
     }
 ) {
     override fun onEnable() {
+        val langDefaultFileName = mapOf<String,String>(
+            "assembly" to "main.asm",
+            "ats" to "main.dats",
+            "bash" to "main.sh",
+            "c" to "main.c",
+            "clojure" to "main.clj",
+            "cobol" to "main.cob",
+            "coffeescript" to "main.coffee",
+            "cpp" to "main.cpp",
+            "crystal" to "main.cr",
+            "csharp" to "main.cs",
+            "d" to "main.d",
+            "elixir" to "main.ex",
+            "elm" to "Main.elm",
+            "erlang" to "main.erl",
+            "fsharp" to "main.fs",
+            "go" to "main.go",
+            "groovy" to "main.groovy",
+            "haskell" to "main.hs",
+            "idris" to "main.idr",
+            "java" to "Main.java",
+            "javascript" to "main.js",
+            "julia" to "main.jl",
+            "kotlin" to "main.kt",
+            "lua" to "main.lua",
+            "mercury" to "main.m",
+            "nim" to "main.nix",
+            "ocaml" to "main.ml",
+            "perl" to "main.pl",
+            "php" to "main.php",
+            "python" to "main.py",
+            "raku" to "main.raku",
+            "swift" to "main.swift",
+            "typescript" to "main.ts",
+            "plaintext" to "main.txt",
+            "nix" to "main.nix",
+            "ruby" to "main.rb",
+            "rust" to "main.rs",
+            "scala" to "scala"
+        )
         logger.info { "Plugin loaded" }
 
         PluginConfig.reload()
@@ -68,12 +109,41 @@ object PluginMain : KotlinPlugin(
                 if (Pattern.compile("^#list").matcher(message.content).find()) {
                     var msg = ""
                     listLang.forEach { lang ->
+                        var fileName : String? = null
                         if (lang is JSONObject) {
-                            msg += "${lang.getString("name")} "
+                            try{
+                                fileName = langDefaultFileName.getValue(lang.getString("name").toString())
+                            } catch (e: NoSuchElementException) {
+                                logger.error(lang.getString("name").toString())
+                            }
+                            msg += "${lang.getString("name")} $fileName\n"
                         }
                     }
 
                     subject.sendMessage(msg)
+                }
+
+                if (Pattern.compile("^#help").matcher(message.content).find()) {
+                    var msg = """
+                        #list 获取支持编程语言列表
+                        #help 获取帮助
+                        #about 获取这个插件的信息
+                    """.trimIndent()
+
+                    subject.sendMessage(msg)
+                }
+
+                if (Pattern.compile("^#about").matcher(message.content).find()) {
+                    var msg = """
+                        mirai-console-glot
+                        github: https://github.com/EromangaMe/mirai-conosle-glot
+                        求打Star[mirai:face:111]
+                        这是一个mirai-console的插件
+                        使用glot的API
+                        https://glot.io/
+                    """.trimIndent()
+
+                    subject.sendMessage(msg.deserializeMiraiCode())
                 }
 
                 listLang.forEach { lang ->
@@ -81,7 +151,7 @@ object PluginMain : KotlinPlugin(
                         val langUrl = lang.getString("url").toString() + "/latest"
                         val langName = lang.getString("name").toString()
 
-                        val p = Pattern.compile(PluginConfig.regex.replace("\$code", langName)).matcher(message.content)
+                        val p = Pattern.compile(PluginConfig.regex.replace("\$land", langName), Pattern.DOTALL).matcher(message.content)
                         if (p.find()) {
                             if (p.groupCount() > 0) {
                                 val code = p.group(1)
@@ -99,10 +169,16 @@ object PluginMain : KotlinPlugin(
                                 runCodeReq.header("Authorization", "Token $token")
                                 runCodeReq.header("Content-type", "application/json")
 
+                                val fileName = langDefaultFileName.getValue(langName)
+                                if(fileName !is String) {
+                                    subject.sendMessage("默认文件名没有")
+                                    return@subscribeAlways
+                                }
+
                                 val codeObject = RequestCode(
                                     listOf(
                                         CodeFile(
-                                            langName,
+                                            fileName,
                                             code
                                         )
                                     )
@@ -122,11 +198,11 @@ object PluginMain : KotlinPlugin(
                                     val error = responseObject.getString("error")
 
                                     if (stdout is String && stdout.isNotEmpty()) {
-                                        subject.sendMessage(stdout)
+                                        subject.sendMessage(stdout.deserializeMiraiCode())
                                     }
 
                                     if (stderr is String && stderr.isNotEmpty()) {
-                                        subject.sendMessage(stderr)
+                                        subject.sendMessage(stderr.deserializeMiraiCode())
                                     }
 
                                     if (error is String && error.isNotEmpty()) {
